0276eddd4707c11adf71460a8181442a7f6c4b32
QaisarWaheed <aliamsari311@gmail.com>
2025-10-27 13:27:57 +0500

    test commit

--- src/features/products/controller/products.controller.ts
+++ src/features/products/controller/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from '../services/product.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from '../dtos/CreateProduct';
@@ -6,12 +14,10 @@ import { CreateProductDto } from '../dtos/CreateProduct';
 @ApiTags('Products')
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productService: ProductService) {}
 
-constructor(private readonly productService: ProductService) {}
-
-
-@Get('/getAll')
-async getAllProducts() {
+  @Get()
+  async getAllProducts() {
     return this.productService.findAll();
   }
 
@@ -20,20 +26,18 @@ async getAllProducts() {
     return this.productService.findById(id);
   }
 
-@Post()  
-    async createProduct(@Body() productData: CreateProductDto) {
+  @Post()
+  async createProduct(@Body() productData: CreateProductDto) {
     return this.productService.addProduct(productData);
   }
 
-  
   @Put('/:id')
   async updateProduct(@Param('id') id: string, @Body() productData: any) {
     return this.productService.updateProduct(id, productData);
   }
 
-    @Delete('/:id')
-    async deleteProduct(@Param('id') id: string) {
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
     return this.productService.deleteProduct(id);
   }
-
 }
